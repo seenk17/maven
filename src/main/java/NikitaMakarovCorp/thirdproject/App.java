ed0f272c0019f712af6b4d0ba261ecb9a65421e7
Nikita Makarov <seenklol17@gmail.com>
2018-03-28 22:15:45 +0700

    compile

--- src/main/java/NikitaMakarovCorp/thirdproject/App.java
+++ src/main/java/NikitaMakarovCorp/thirdproject/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	int v;
+    	v=2*2;
+        System.out.println( "Hello World="+ v );
     }
 }
